bfde6f7008eb3e6655d00723af2fabacb3c35e30
Jboggs64 <51323689+Jboggs64@users.noreply.github.com>
2024-02-05 19:16:08 -0500

    Basic battle mechanics
    Without the UI it doesn't actually function right now but the code is there to be able to run a basic turn based battle once we populate certain text boxes onto the UI for Attack and Heal. Player is a bit buggy right now so I might just switch it out for basic cylinders until I figure that out

---
+++ Assets/Scripts/Battle Mechanics/BattleSystem.cs
@@ -0,0 +1,145 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public enum BattleState { START, PLAYERTURN, ENEMYTURN, WON, LOST }
+
+public class BattleSystem : MonoBehaviour
+{
+    public GameObject playerPrefab;
+    public GameObject enemyPrefab;
+
+    public Transform playerBattleStation;
+    public Transform enemyBattleStation;
+
+    Unit playerUnit;
+    Unit enemyUnit;
+
+    public Text dialogueText;
+
+    public BattleState state;
+    // Start is called before the first frame update
+    void Start()
+    {
+        state = BattleState.START;
+        StartCoroutine(SetupBattle());
+    }
+
+    IEnumerator SetupBattle()
+    {
+        GameObject playerGO = Instantiate(playerPrefab, playerBattleStation);
+        playerUnit = playerGO.GetComponent<Unit>();
+
+        GameObject enemyGO = Instantiate(enemyPrefab, enemyBattleStation);
+        enemyUnit = enemyGO.GetComponent<Unit>();
+
+        dialogueText.text = " A wild " + enemyUnit.unitName + " approaches";
+
+        yield return new WaitForSeconds(4f);
+
+        state = BattleState.PLAYERTURN;
+        PlayerTurn();
+    }
+
+    void PlayerTurn()
+    {
+        
+
+        dialogueText.text = "Choose an action:";
+
+        
+    }
+
+    public void OnAttackButton()
+    {
+        if (state != BattleState.PLAYERTURN)
+        {
+            return;
+        }
+
+        StartCoroutine(PlayerAttack());
+    }
+
+    public void OnHealButton()
+    {
+        if (state != BattleState.PLAYERTURN)
+        {
+            return;
+        }
+
+        StartCoroutine(PlayerHeal());
+    }
+
+    IEnumerator PlayerAttack()
+    {
+        // Damage enemy
+        bool isDead = enemyUnit.TakeDamage(playerUnit.damage);
+
+        // Set HUD
+        dialogueText.text = "The attack is successful!";
+
+        yield return new WaitForSeconds(2f);
+
+        // Check if enemy is dead
+        if (isDead)
+        {
+            // End battle
+            state = BattleState.WON;
+            EndBattle();
+        }
+        else
+        {
+            // Enemys turn
+            state = BattleState.ENEMYTURN;
+            StartCoroutine(EnemyTurn());
+        }
+
+        // Change state accordingly
+    }
+
+    IEnumerator PlayerHeal()
+    {
+        playerUnit.Heal(5);
+        // Set HUD
+        dialogueText.text = "You were healed!";
+
+        yield return new WaitForSeconds(2f);
+
+        state = BattleState.ENEMYTURN;
+        StartCoroutine(EnemyTurn());
+    }
+
+    IEnumerator EnemyTurn()
+    {
+        dialogueText.text = enemyUnit.unitName + " attacks!";
+        yield return new WaitForSeconds(2f);
+
+        bool isDead = playerUnit.TakeDamage(enemyUnit.damage);
+        // Set HUD
+        
+        if (isDead)
+        {
+            state = BattleState.LOST;
+            EndBattle();
+        } else
+        {
+            state = BattleState.PLAYERTURN;
+            PlayerTurn();
+        }
+    }
+
+    void EndBattle()
+    {
+        if (state == BattleState.WON)
+        {
+            dialogueText.text = "You won!";
+        } else if (state == BattleState.LOST)
+        {
+            dialogueText.text = "You were defeated...";
+        }
+    }
+
+    
+
+}
