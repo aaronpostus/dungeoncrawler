c8e13ab2bb9774e01b891ed76b13097f4c5add78
Aaron Post <aarontpost@gmail.com>
2024-03-02 15:00:54 -0500

    Revert "huge rebase"
    This reverts commit dd2f44dfa0d38ca704626a6ccdb54a986b284290.

--- Assets/Scripts/Puzzles/ElectricityPuzzle.cs
+++ Assets/Scripts/Puzzles/ElectricityPuzzle.cs
@@ -125,7 +125,7 @@ public class ElectricityPuzzle : MonoBehaviour
         Debug.LogWarning("Max attempts reached to find a valid position. Returning a position regardless.");
         return randomPosition;
     }
-    private void OnDrawGizmos()
+    private void OnDrawGizmosSelected()
     {
         if (boundsMin != null && boundsMax != null)
         {
