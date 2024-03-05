afd5b557918c9e10cdd0a19b48657d7c2efdd2fc
Aaron Post <aarontpost@gmail.com>
2024-03-04 20:42:33 -0500

    LETS FUCKING GO

---
+++ Assets/Scripts/Puzzles/PuzzleData/PuzzleData.cs
@@ -0,0 +1,10 @@
+﻿
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "PuzzleData", menuName = "ScriptableObjects/PuzzleData")]
+public class PuzzleData : ScriptableObject
+{
+    // number from 1 to 5
+    public int difficulty;
+    public bool solved = false;
+}
