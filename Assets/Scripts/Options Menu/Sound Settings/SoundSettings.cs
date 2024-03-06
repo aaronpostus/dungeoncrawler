6a6be5a173eee9ab07856993174f3b10b2b4a591
amjason233 <amjason233@gmail.com>
2024-03-06 16:04:45 -0500

    Options Sound Added/UI sound updated

--- Assets/Scripts/Options Menu/Sound Settings/SoundSettings.cs
+++ Assets/Scripts/Options Menu/Sound Settings/SoundSettings.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
+using System.Runtime.CompilerServices;
 
 public class SoundSettings : MonoBehaviour
 {
@@ -19,53 +20,81 @@ public class SoundSettings : MonoBehaviour
     [SerializeField] private GameObject confirmationPrompt;
     [SerializeField] private GameObject resetPrompt;
 
-    private float defaultValue = 50f;
 
-    public void SetMasterVolume(float volume)
+    private FMOD.Studio.Bus Master;
+    private FMOD.Studio.Bus Music;
+    private FMOD.Studio.Bus SFX;
+    private float MusicVolume;
+    private float SFXVolume;
+    private float MasterVolume;
+
+    private float defaultValue = 0.5f;
+
+    void Awake()
     {
-        //change volume in game with FMOD?
-        AudioListener.volume = volume / 100;
-
-        masterTextValue.text = volume.ToString("0");
+        Music = FMODUnity.RuntimeManager.GetBus("bus:/Music");
+        SFX = FMODUnity.RuntimeManager.GetBus("bus:/SFX");
+        Master = FMODUnity.RuntimeManager.GetBus("bus:/");
+        MusicVolume = defaultValue;
+        SFXVolume = defaultValue;
+        MasterVolume = 1f;
     }
 
-    public void SetMusicVolume(float volume)
+    public void SetMasterVolume(float newMasterVolume)
     {
-        //change volume in game with FMOD?
+        MasterVolume = newMasterVolume / 100f;
 
-        musicTextValue.text = volume.ToString("0");
+        masterTextValue.text = newMasterVolume.ToString("0");
     }
 
-    public void SetSoundEffectsVolume(float volume)
+    public void SetMusicVolume(float newMusicVolume)
     {
-        //change volume in game with FMOD?
+        MusicVolume = newMusicVolume / 100f;
 
-        soundEffectsTextValue.text = volume.ToString("0");
+        musicTextValue.text = newMusicVolume.ToString("0");
     }
 
+    public void SetSoundEffectsVolume(float newSFXVolume)
+    {
+        SFXVolume = newSFXVolume / 100f;
+
+        soundEffectsTextValue.text = newSFXVolume.ToString("0");
+    }
 
     public void ResetButtonClicked()
     {
         //resets master volume
-        AudioListener.volume = defaultValue / 100;
-        masterTextValue.text = defaultValue.ToString("0");
-        masterVolumeSlider.value = defaultValue;
+        Master.setVolume(defaultValue);
+        masterTextValue.text = (defaultValue * 100).ToString("0");
+        masterVolumeSlider.value = (defaultValue * 100);
 
         //resets music volume
-        musicTextValue.text = defaultValue.ToString("0");
-        musicVolumeSlider.value = defaultValue;
+        Music.setVolume(defaultValue);
+        musicTextValue.text = (defaultValue * 100).ToString("0");
+        musicVolumeSlider.value = (defaultValue * 100);
 
         //resets sound effect volume
-        soundEffectsTextValue.text = defaultValue.ToString("0");
-        soundEffectsSlider.value = defaultValue;
+        SFX.setVolume(defaultValue);
+        soundEffectsTextValue.text = (defaultValue * 100).ToString("0");
+        soundEffectsSlider.value = (defaultValue * 100);
 
-        PlayerPrefs.SetFloat("masterVolume", AudioListener.volume);
+        PlayerPrefs.SetFloat("masterVolume", defaultValue);
+        PlayerPrefs.SetFloat("musicVolume", defaultValue);
+        PlayerPrefs.SetFloat("soundEffectsVolume", defaultValue);
+
         StartCoroutine(ResetBox());
     }
 
     public void VolumeApply()
     {
-        PlayerPrefs.SetFloat("masterVolume", AudioListener.volume);
+        Master.setVolume(MasterVolume);
+        Debug.Log(MasterVolume.ToString());
+        Music.setVolume(MusicVolume);
+        SFX.setVolume(SFXVolume);
+
+        PlayerPrefs.SetFloat("masterVolume", MasterVolume);
+        PlayerPrefs.SetFloat("musicVolume", MusicVolume);
+        PlayerPrefs.SetFloat("soundEffectsVolume", SFXVolume);
 
         StartCoroutine(ConfirmationBox());
     }
