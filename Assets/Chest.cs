fbec372263a78e66306ed4039f190e3548878113
Aaron Post <aarontpost@gmail.com>
2024-03-06 18:09:21 -0500

    wip hotfixes

--- Assets/Chest.cs
+++ Assets/Chest.cs
@@ -6,13 +6,23 @@ public class Chest : MonoBehaviour
 {
     [SerializeField] PuzzleData puzzleData;
     [SerializeField] Animator anim;
+    [SerializeField] StringReference chestUnlocked;
     [SerializeField] GameObject triggerBox;
     // Start is called before the first frame update
     void Awake()
     {
         if (puzzleData.solved) {
             anim.SetBool("ChestOpen", true);
+            chestUnlocked.Value = "Opened chest and unlocked item.";
             Destroy(triggerBox);
+            StartCoroutine(RemoveText());
         }
+    }
+    IEnumerator RemoveText() {
+        yield return new WaitForSeconds(2f);
+        if (chestUnlocked.Value == "chestUnlocked.Value") {
+            chestUnlocked.Value = "";
+        }
+        yield return null;
     }
 }
