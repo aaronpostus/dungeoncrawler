8f7f6a5155d2bd16f4845fc0c738b47ad1cc8fea
Aaron Post <aarontpost@gmail.com>
2024-03-28 15:40:46 -0400

    Added Give Up Button for slide puzzle

--- Assets/Scripts/Puzzles/LoadSceneEvent.cs
+++ Assets/Scripts/Puzzles/LoadSceneEvent.cs
@@ -8,7 +8,12 @@ namespace Assets.Scripts.Puzzles
     internal class LoadSceneEvent : InteractionEvent
     {
         [SerializeField] string sceneName;
+        [SerializeField] bool returnToMainScene;
         public override void Interact() {
+            if (returnToMainScene) {
+                SaveGameManager.instance.ReturnToMainScene();
+                return;
+            }
             SaveGameManager.instance.TransitionAwayFromMainScene(sceneName);
         }
     }
